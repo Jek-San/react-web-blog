6a8fa95590cd6db35a9b80bd9d2fbdabe9f87208
Jek-San <88803418+Jek-San@users.noreply.github.com>
2023-08-28 05:46:41 +0700

    done with page single, page home, and page write

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,35 @@
+import "./post.css"
+
+export default function Post() {
+  return (
+    <div className="post">
+      <img
+        className="postImg"
+        src=" https://images.pexels.com/photos/10208449/pexels-photo-10208449.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt="postImg"
+      />
+      <div className="postInfo">
+        <div className="postCats">
+          <span className="postCat">Music</span>
+          <span className="postCat">Life</span>
+        </div>
+        <span className="postTitle">Lorem ipsum dolor sit amet.</span>
+        <hr />
+        <span className="postDate">1 hour ago</span>
+      </div>
+      <p className="postDesc">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat
+        impedit odio ab illum cumque, debitis in quasi quis natus voluptas
+        itaque sunt error ea optio similique animi id aliquam. Aut. ipsum dolor
+        sit amet, consectetur adipisicing elit. Placeat impedit odio ab illum
+        cumque, debitis in quasi quis natus voluptas itaque sunt error ea optio
+        similique animi id aliquam. Aut. ipsum dolor sit amet, consectetur
+        adipisicing elit. Placeat impedit odio ab illum cumque, debitis in quasi
+        quis natus voluptas itaque sunt error ea optio similique animi id
+        aliquam. Aut. ipsum dolor sit amet, consectetur adipisicing elit.
+        Placeat impedit odio ab illum cumque, debitis in quasi quis natus
+        voluptas itaque sunt error ea optio similique animi id aliquam. Aut.
+      </p>
+    </div>
+  )
+}
---
+++ src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,75 @@
+import "./singlePost.css"
+
+export default function SinglePost() {
+  return (
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        <img
+          className="singlePostImg"
+          src=" https://images.pexels.com/photos/17824719/pexels-photo-17824719/free-photo-of-burung-air-hewan-aliran.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <h1 className="singlePostTitle">
+          Lorem ipsum dolor sit amet.
+          <div className="singlePostEdit">
+            <i className=" singlePostIcon fa-regular fa-pen-to-square"></i>
+            <i className=" singlePostIcon fa-regular fa-trash-can"></i>
+          </div>
+        </h1>
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">
+            Author: <b>Jack</b>
+          </span>
+          <span className="singlePostDate">1 hour ago</span>
+        </div>
+        <p className="singlePostDesc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis alias
+          amet expedita at ab fugit vel id vero impedit dolore eaque ipsa nisi,
+          cupiditate enim possimus molestias voluptas, sint ullam! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Quis alias amet expedita
+          at ab fugit vel id vero impedit dolore eaque ipsa nisi, cupiditate
+          enim possimus molestias voluptas, sint ullam! Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Quis alias amet expedita at ab
+          fugit vel id vero impedit dolore eaque ipsa nisi, cupiditate enim
+          possimus molestias voluptas, sint ullam! Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Quis alias amet expedita at ab fugit vel
+          id vero impedit dolore eaque ipsa nisi, cupiditate enim possimus
+          molestias voluptas, sint ullam! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quis alias amet expedita at ab fugit vel id vero
+          impedit dolore eaque ipsa nisi, cupiditate enim possimus molestias
+          voluptas, sint ullam! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quis alias amet expedita at ab fugit vel id vero
+          impedit dolore eaque ipsa nisi, cupiditate enim possimus molestias
+          voluptas, sint ullam! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quis alias amet expedita at ab fugit vel id vero
+          impedit dolore eaque ipsa nisi, cupiditate enim possimus molestias
+          voluptas, sint ullam! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quis alias amet expedita at ab fugit vel id vero
+          impedit dolore eaque ipsa nisi, cupiditate enim possimus molestias
+          voluptas, sint ullam! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quis alias amet expedita at ab fugit vel id vero
+          impedit dolore eaque ipsa nisi, cupiditate enim possimus molestias
+          voluptas, sint ullam! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quis alias amet expedita at ab fugit vel id vero
+          impedit dolore eaque ipsa nisi, cupiditate enim possimus molestias
+          voluptas, sint ullam! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quis alias amet expedita at ab fugit vel id vero
+          impedit dolore eaque ipsa nisi, cupiditate enim possimus molestias
+          voluptas, sint ullam! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quis alias amet expedita at ab fugit vel id vero
+          impedit dolore eaque ipsa nisi, cupiditate enim possimus molestias
+          voluptas, sint ullam! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quis alias amet expedita at ab fugit vel id vero
+          impedit dolore eaque ipsa nisi, cupiditate enim possimus molestias
+          voluptas, sint ullam! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quis alias amet expedita at ab fugit vel id vero
+          impedit dolore eaque ipsa nisi, cupiditate enim possimus molestias
+          voluptas, sint ullam! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quis alias amet expedita at ab fugit vel id vero
+          impedit dolore eaque ipsa nisi, cupiditate enim possimus molestias
+          voluptas, sint ullam!
+        </p>
+      </div>
+    </div>
+  )
+}
